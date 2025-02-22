b7f30f610d31d770321fae555ae227bd1eb4961f
cool-guys-bfc2 <lamarasingham@icloud.com>
2025-02-22 10:22:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 import os
-from flask import Flask
+from flask import Flask,request
 socket=[]
 app=Flask(__name__)
 
-@app.route("/socket/<request>")
-def run(request):
-  socket.append(request)
+@app.route("/socket/<req>")
+def run(req):
+  socket.append(req)
   return "Done!"
 
 @app.route("/get/")
 def get():
   return " ".join(socket)
+
+@app.route("/db/write/<fn>")
+def write(fn):
+  fc=request.args.get("content")
+  fh=open(fn,"w")
+  fh.write(fc)
+  fh.close()
+  return "Done!"
+
+@app.route("db/read
 
 app.run(host="0.0.0.0",port=int(os.environ.get("PORT")))
